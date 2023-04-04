cb5098e74282c308b8270cc7f175b9e9460c7af6
Raushan-Shahi <raushanshahi2001@gmail.com>
2023-04-05 00:36:36 +0530

    boolean in conditional statements

--- boolean_conversion.py
+++ boolean_conversion.py
@@ -1,5 +1,3 @@
-# We can use non-boolean objects in if conditions and other places where a boolean would be expected. 
-# Python will implicitly treat them as their corresponding boolean value:
 
 
 print(bool(1)) # all numbers are treated as true, except 0
@@ -9,3 +7,15 @@ print(bool(""))
 # Generally empty sequences (strings, lists, and other types we've yet to see like lists and tuples)
 # are "falsey" and the rest are "truthy"
 print(bool(-56))
+
+
+
+
+# We can use non-boolean objects in if conditions and other places where a boolean would be expected. 
+# Python will implicitly treat them as their corresponding boolean value:
+
+
+if 0:                           #0 is a false statement so if condition will be skipped 
+    print(0)
+elif "spam":                    #"spam" is a true boolean so it will be executed 
+    print("spam")
