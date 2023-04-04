1690eafe4b2412efab962fc4ebbe86e584570bea
Raushan-Shahi <raushanshahi2001@gmail.com>
2023-04-05 00:32:59 +0530

    comment added

--- boolean_conversion.py
+++ boolean_conversion.py
@@ -1,3 +1,7 @@
+# We can use non-boolean objects in if conditions and other places where a boolean would be expected. 
+# Python will implicitly treat them as their corresponding boolean value:
+
+
 print(bool(1)) # all numbers are treated as true, except 0
 print(bool(0))
 print(bool("asf")) # all strings are treated as true, except the empty string ""
